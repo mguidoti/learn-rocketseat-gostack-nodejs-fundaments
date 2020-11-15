85a86bb565f03b6dcb37c4e55d011de96378ee7f
mguidoti <marcus.guidoti@gmail.com>
2020-11-14 23:40:57 -0300

    feat: Add all, create to TransactionsRepository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,7 +6,14 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
+
   private transactions: Transaction[];
 
   constructor() {
@@ -15,14 +22,55 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const { income, outcome } = this.transactions.reduce((acc: Balance, transaction: Transaction) => {
+      switch (transaction.type) {
+        case 'income':
+          acc.income += transaction.value;
+          break;
+        case 'outcome':
+          acc.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+
+      return acc;
+
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total
+    };
+
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: TransactionDTO): Transaction {
     // TODO
+
+    const { total } = this.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('This costs more than you have.');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
